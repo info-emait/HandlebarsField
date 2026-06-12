5b20c1b6891c3e82743cdc59394479a66455d65c
Šutka Martin <info.emait@gmail.com>
2026-06-12 11:07:42 +0200

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig, loadEnv } from "vite";
+import { resolve } from "path";
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    const isDebug = mode === "debug";
+
+    return {
+        base: env.VITE_APP_BASE_URL || "./",
+        build: {
+            outDir: "dist",
+            emptyOutDir: true,
+            sourcemap: isDebug,
+            minify: isDebug ? false : "esbuild",
+            cssMinify: isDebug ? false : "esbuild",
+            rolldownOptions: {
+                input: {
+                    "field": resolve(__dirname, "field.html")
+                }
+            }
+        },
+        resolve: {
+            alias: {
+                "@": resolve(__dirname, "src"),
+                "@utils": resolve(__dirname, "src/utils"),
+                "@styles": resolve(__dirname, "src/styles")
+            }
+        },
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    quietDeps: true
+                }
+            }
+        }
+    };
+});
